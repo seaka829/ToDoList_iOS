c1119f0d4eb6d7e78bfd626bf7943e3b37213388
Ryota Akase <ryota@RyotanoMacBook-Pro.local>
2019-12-11 01:52:04 +0900

    getでapiからデータ取得 table viewへの表示

--- ToDoList_iOS/ViewController.swift
+++ ToDoList_iOS/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var jsonMap: [[String: Any]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let url: URL = URL(string: "http://52.203.253.84:8080/api/v1/tasks")!
+        let task: URLSessionTask  = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as! [Any]
+                self.jsonMap = json.map { (jsonMap) -> [String: Any] in
+                    return jsonMap as! [String: Any]
+                }
+                self.tableView.reloadData()
+                print(self.jsonMap)
+            }
+            catch {
+                // 例外処理を書く
+                print(error)
+            }
+        })
+        task.resume()
     }
-
-
+    
+    // セルの設定
+    func tableView(_ table: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let taskNameLbl = cell.viewWithTag(2) as! UILabel
+        let deadlineLbl = cell.viewWithTag(3) as! UILabel
+        
+        taskNameLbl.text = jsonMap[indexPath.row]["TaskName"] as? String
+        deadlineLbl.text = jsonMap[indexPath.row]["Deadline"] as? String
+        
+        return cell
+    }
+    
+    // セル数の設定
+    func tableView(_ table: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return jsonMap.count
+    }
+    
+    // セルの高さを設定
+    func tableView(_ table: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90.0
+    }
 }
 
